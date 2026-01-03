88b641fea17172f1786e418b0f98b40818f8b8a9
Sergei Gritsun <gritsun.sp@gmail.ru>
2026-01-04 00:23:14 +0300

    TK 010: CommentSerializer is improved. Output matches the documentation

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -139,11 +139,10 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    review = serializers.SlugRelatedField(slug_field='id', read_only=True)
     author = serializers.SlugRelatedField(
         slug_field='username', read_only=True
     )
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('id', 'text', 'author', 'pub_date')
