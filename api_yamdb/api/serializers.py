b5741a3c8c34c3cc26594798c6d86e8b33339487
Sergei Gritsun <gritsun.sp@gmail.ru>
2026-01-03 13:19:41 +0300

    TK 001:Fix bug with serializers.SerializerMethodField()

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -94,7 +94,7 @@ class GenreSerializer(serializers.ModelSerializer):
 class TitleReadSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     genre = GenreSerializer(read_only=True, many=True)
-    rating = serializers.SerializerMethodField()
+    rating = serializers.FloatField(read_only=True)
 
     def get_rating(self, obj):
         if hasattr(obj, 'rating') and obj.rating is not None:
